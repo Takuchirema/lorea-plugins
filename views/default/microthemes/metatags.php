9d7e157d8cd54fe2f608ded72e680c3fc9ed9e12
Pablo Martin <caedes@doodo.org>
2010-05-07 09:19:10 +0200

    fix using microtheme when the theme has been deleted or is inaccessible for any other reason.

--- views/default/microthemes/metatags.php
+++ views/default/microthemes/metatags.php
@@ -3,22 +3,22 @@
 	$page_owner = page_owner_entity();
 	$owner = null;
 	if ((($page_owner instanceof ElggUser && (get_context() == "profile" || get_context() == "microthemes" || get_context() == "dashboard")) || 
-	    ($page_owner instanceof ElggGroup)) && $page_owner->microtheme) {
+	    ($page_owner instanceof ElggGroup)) && $page_owner->microtheme && get_entity($page_owner->microtheme)) {
 		$owner = $page_owner;
 	}
 	else if (get_context() == "microthemes") {
 		$owner = get_entity(get_input("assign_to"));
 		if (!$owner) {
 			$user = get_loggedin_user();
-			if ($user->microtheme)
-				$owner = get_loggedin_user();
+			if ($user->microtheme && get_entity($user->microtheme))
+				$owner = $user;
 		}
 	}
 	if (!$owner) {
 		$site = get_site_by_url($CONFIG->wwwroot);
 		$owner = $site;
 	}
-	if ($owner && $owner->microtheme) {
+	if ($owner && $owner->microtheme && get_entity($owner->microtheme)) {
 ?>
 	<link rel="stylesheet" href="<?php echo $vars['url']; ?>pg/microthemes/css&guid=<?php echo $owner->getGUID(); ?>" type="text/css" />
 <?php
