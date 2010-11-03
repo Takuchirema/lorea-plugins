ddf2a6bc83ed3d61f6a3e5f27e079fe896c1468d
Pablo Martin <caedes@doodo.org>
2010-11-03 21:52:09 +0100

    language update

--- languages/es.php
+++ languages/es.php
@@ -1,48 +1,39 @@
 <?php
-/**
-         * Elgg spotlight lorea
-         * 
-         * @package
-         * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-         * @author lorea
-         * @copyright lorea
-         * @link http://lorea.cc
-         */
 
-	$spanish = array(
-                    "microthemes:view" => 'Microtemas',
-                    "microthemes:choosesite" => 'Seleccionar como tema del sitio',
-                    "microthemes:create" => 'Crear un tema',
-                    "microthemes:edit" => 'Editar',
-		    "microthemes:choose" => 'Seleccionar',
-                    "microthemes:title" => 'Titulo',
-		    "microthemes:translucid_page" => 'Hacer el fondo de las paginas translucido.',
-                    "microthemes:themeinstructions" => 'Aqui ves los temas disponibles y puedes seleccionar alguno que te guste',
-                    "microthemes:color" => 'Color de fondo',
-                    "microthemes:topbar_color" => 'Color para la barra',
-                    "microthemes:banner" => 'Imagen de cabecera',
-                    "microthemes:groupthemes" => 'Tema de grupo',
-                    "microthemes:userthemes" => 'Tema personal',
-                    "microthemes:headerheight" => 'Altura de la cabecera',
-                    "microthemes:bgrepetition" => 'Repeticion del fondo',
-                    "microthemes:repeatx" => 'repetir horizontalmente',
-                    "microthemes:repeaty" => 'repetir verticalmente',
-                    "microthemes:alignment" => 'Alineamiento del fondo',
-                    "microthemes:clearsite" => 'Quitar el tema del sitio',
-                    "microthemes:clearuser" => 'Quitar tu tema',
-                    "microthemes:cleargroup" => 'Quitar el tema del grupo',
-                    "microthemes:clear:failed" => 'error quitando el tema',
-                    "microthemes:left" => 'izquierda',
-                    "microthemes:center" => 'centro',
-                    "microthemes:right" => 'derecha',
-                    "microthemes:options" => 'Opciones extra',
-                    "microthemes:hidesitename" => 'Ocultar el nombre del sitio en la parte superior de la pagina',
-		    "item:object:microtheme" => 'Temas',
+// Generado por translationbrowser 
 
-                    "microthemes:publish" => 'Guardar',
+$spanish = array( 
+	 'microthemes:view'  =>  "Microtemas" , 
+	 'microthemes:choosesite'  =>  "Seleccionar como tema del sitio" , 
+	 'microthemes:create'  =>  "Crear un tema" , 
+	 'microthemes:edit'  =>  "Editar" , 
+	 'microthemes:choose'  =>  "Seleccionar" , 
+	 'microthemes:title'  =>  "Título" , 
+	 'microthemes:translucid_page'  =>  "Hacer el fondo de las páginas translúcido." , 
+	 'microthemes:themeinstructions'  =>  "Aquí ves los temas disponibles y puedes seleccionar alguno que te guste" , 
+	 'microthemes:color'  =>  "Color de fondo" , 
+	 'microthemes:topbar_color'  =>  "Color para la barra" , 
+	 'microthemes:banner'  =>  "Imagen de cabecera" , 
+	 'microthemes:groupthemes'  =>  "Tema de grupo" , 
+	 'microthemes:userthemes'  =>  "Tema personal" , 
+	 'microthemes:headerheight'  =>  "Altura de la cabecera" , 
+	 'microthemes:bgrepetition'  =>  "Repetición del fondo" , 
+	 'microthemes:repeatx'  =>  "repetir horizontalmente" , 
+	 'microthemes:repeaty'  =>  "repetir verticalmente" , 
+	 'microthemes:alignment'  =>  "Alineamiento del fondo" , 
+	 'microthemes:clearsite'  =>  "Quitar el tema del sitio" , 
+	 'microthemes:clearuser'  =>  "Quitar tu tema actual" , 
+	 'microthemes:cleargroup'  =>  "Quitar el tema del grupo" , 
+	 'microthemes:clear:failed'  =>  "error quitando el tema" , 
+	 'microthemes:left'  =>  "izquierda" , 
+	 'microthemes:center'  =>  "centro" , 
+	 'microthemes:right'  =>  "derecha" , 
+	 'microthemes:options'  =>  "Opciones extra" , 
+	 'microthemes:hidesitename'  =>  "Ocultar el nombre del sitio en la parte superior de la página" , 
+	 'item:object:microtheme'  =>  "Temas" , 
+	 'microthemes:publish'  =>  "Guardar"
+); 
 
-	);
-	
-	add_translation("es",$spanish);
+add_translation('es', $spanish); 
 
 ?>
