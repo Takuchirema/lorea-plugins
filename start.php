bccc3f9adf6d29e8212d7b4889352a2d96d641dd
Sem <sembrestels@riseup.net>
2011-10-28 16:43:57 +0200

    Overriding elgg core footer

--- start.php
+++ start.php
@@ -12,8 +12,6 @@ elgg_register_event_handler('init', 'system', 'powered_init');
  */
 function powered_init(){
 	elgg_extend_view('css/elgg','powered/css');
-	// Extend footer
-	elgg_extend_view("footer/analytics", "powered/footer");
 }
 
 
