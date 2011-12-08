87110ddf3cc8e467aa8340df4d8a094034c76ea0
Sem <sembrestels@riseup.net>
2011-12-08 01:34:15 +0100

    Translations.

--- views/default/page/elements/spotlight.php
+++ views/default/page/elements/spotlight.php
@@ -5,64 +5,71 @@ echo '<div class="spotlight clearfloat">';
 echo '<div class="spotlight-column">';
 
 echo elgg_view('page/elements/spotlight/module', array(
-	'title' => 'Notícies',
+	'title' => elgg_echo('news:news'),
 	'items' => array(
-		'https://n-1.cc/pg/pages/view/9385/' => 'Noves eines',
+		'https://n-1.cc/pg/pages/view/9385/' => elgg_echo('news:features'),
 	),
 ));
 
 echo elgg_view('page/elements/spotlight/module', array(
-	'title' => 'Lorea',
+	'title' => elgg_echo('about:lorea'),
 	'items' => array(
-		'https://lorea.org/' => 'Pàgina d\'inici',
-		'https://n-1.cc/pg/groups/7826/lorea/' => 'Grup de treball',
-		'https://n-1.cc/pg/pages/view/14884/' => 'Donacions',
-	),
-));
-
-echo '</div><div class="spotlight-column">';
-
-echo elgg_view('page/elements/spotlight/module', array(
-	'title' => 'Contacte',
-	'items' => array(
-		'https://lists.rhizomatik.net/listinfo/mycelia-community' => 'Llista de correu',
-	),
-));
-
-echo elgg_view('page/elements/spotlight/module', array(
-	'title' => 'Ajuda',
-	'items' => array(
-		'https://n-1.cc/pg/faq/' => 'FAQ/Contacte',
-		'https://n-1.cc/pg/dokuwiki/9394' => 'Com fer',
-		'https://n-1.cc/pg/groups/9394/help/' => 'Grup d\'ajuda',
+		'https://lorea.org/' => elgg_echo('about:blog'),
+		'https://n-1.cc/pg/groups/7826/lorea/' => elgg_echo('about:group'),
+		elgg_echo('lorea:sustainability:url') => elgg_echo('lorea:sustainability'),
 	),
 ));
 
 echo '</div><div class="spotlight-column">';
 
 echo elgg_view('page/elements/spotlight/module', array(
-	'title' => 'Desenvolupament',
+	'title' => elgg_echo('contact:contact'),
 	'items' => array(
-		'https://n-1.cc/pg/groups/6217/bughunting/' => 'Caçadors de bugs',
-		'https://n-1.cc/pg/groups/5241/testers-de-la-red-social/' => 'Tastaolletes',
-		'https://dev.lorea.org/' => 'Xarxa de desenvolupament',
-		'https://github.com/lorea/'=> 'Repositori',
+		'https://lists.rhizomatik.net/listinfo/mycelia-community' => elgg_echo('contact:mailing'),
+	),
+));
+
+echo elgg_view('page/elements/spotlight/module', array(
+	'title' => elgg_echo('help:help'),
+	'items' => array(
+		'https://n-1.cc/pg/faq/' => elgg_echo('help:faq'),
+		'https://n-1.cc/pg/dokuwiki/9394' => elgg_echo('help:howto'),
+		'https://n-1.cc/pg/groups/9394/help/' => elgg_echo('help:group'),
+	),
+));
+
+echo '</div><div class="spotlight-column">';
+
+echo elgg_view('page/elements/spotlight/module', array(
+	'title' => elgg_echo('dev:dev'),
+	'items' => array(
+		'https://n-1.cc/pg/groups/6217/bughunting/' => elgg_echo('dev:bughunting'),
+		'https://n-1.cc/pg/groups/5241/testers-de-la-red-social/' => elgg_echo('dev:testers'),
+		'https://dev.lorea.org/' => elgg_echo('dev:network'),
+		'https://github.com/lorea/'=> elgg_echo('dev:repo'),
 		
 	),
 ));
 
 echo '</div><div class="spotlight-column">';
 
+$members = get_number_users();
+$online  = find_active_users(600, 0, 0, true);
+$groups  = elgg_get_entities(array('type' => 'group', 'count' => true, 'limit' => 0));
+$pages   = elgg_get_entities(array('type' => 'object', 'subtypes' => array('page', 'page_top', 'etherpad', 'subpad'), 'count' => true, 'limit' => 0));
+$blog    = elgg_get_entities(array('type' => 'object', 'subtype' => 'blog', 'count' => true, 'limit' => 0));
+$file    = elgg_get_entities(array('type' => 'object', 'subtype' => 'file', 'count' => true, 'limit' => 0));
+
 echo elgg_view('page/elements/spotlight/module', array(
-	'title' => 'Estadístiques',
+	'title' => elgg_echo('stats:stats'),
 	'items' => array(
-		'members' => 'membres',
-		'members/online' => 'membres actius',
-		'groups/all' => 'grups',
-		'pages/all' => 'pàgines',
-		'blog/all' => 'blocs',
-		'file/all' => 'fitxers',
-		'tidypics/all' => 'fotos',
+		'members'        => $members . ' ' . elgg_echo('members'),
+		'members/online' => $online . ' ' .  elgg_echo('members:label:online'),
+		'groups/all'     => $groups . ' ' .  elgg_echo('item:group'),
+		'pages/all'      => $pages . ' ' .   elgg_echo('item:object:page'),
+		'blog/all'       => $blog . ' ' .    elgg_echo('item:object:blog'),
+		'file/all'       => $file . ' ' .    elgg_echo('item:object:file'),
+		//'tidypics/all' => $photos . ' ' .  elgg_echo('item:object:photo'),
 	),
 ));
 
