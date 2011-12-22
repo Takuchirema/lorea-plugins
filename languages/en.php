40ead67cddf4212debbca6da311266712a6d048d
Sem <sembrestels@riseup.net>
2011-12-22 19:08:06 +0100

    Added menu items.

---
+++ languages/en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Members English language file
+ */
+
+$english = array(
+	'microthemes' => 'Microthemes',
+	'microthemes:profile' => 'Personal theme',
+	'microthemes:profile:edit' => 'Edit personal theme',
+	'microthemes:group' => 'Group theme',
+	'microthemes:group:edit' => 'Edit group theme',
+);
+
+add_translation('en', $english);
--- start.php
+++ start.php
@@ -14,6 +14,10 @@ function microthemes_init() {
 	// register a library of helper functions
 	elgg_register_library('elgg:microthemes', elgg_get_plugins_path() . 'microthemes/lib/microthemes.php');
 
+	// add page menu items
+	elgg_register_event_handler('pagesetup', 'system', 'microthemes_pagesetup');
+	elgg_register_plugin_hook_handler('register', 'menu:user_hover', 'microthemes_user_hover_menu');
+
 	// Extend system CSS with our own styles, which are defined in the microthemes/css view
 	elgg_extend_view('css/elgg', 'microthemes/css');
 	elgg_extend_view('js/elgg', 'microthemes/js');
@@ -59,3 +63,32 @@ function microthemes_page_handler($page) {
 	return true;
 }
 
+function microthemes_pagesetup() {
+	$owner = elgg_get_page_owner_entity();
+	if ((elgg_instanceof($owner, 'user') || elgg_instanceof($owner, 'group'))
+														&& $owner->canEdit()) {
+		elgg_register_menu_item('page', array(
+			'name' => 'choose_profile_microtheme',
+			'href' => "microthemes/view/" . $owner->username,
+			'text' => elgg_echo('microthemes:profile:edit'),
+			'contexts' => array('profile_edit'),
+		));
+		elgg_register_menu_item('page', array(
+			'name' => 'microthemes',
+			'text' => elgg_echo('microthemes:group:edit'),
+			'href' => "microthemes/view/" . $owner->guid,
+			'context' => array('groups'),
+		));
+	}
+}
+
+function microthemes_user_hover_menu($hook, $type, $return, $params) {
+	$user = $params['entity'];
+	if (elgg_is_logged_in() && elgg_get_logged_in_user_guid() == $user->guid) {
+		$url = "microthemes/$user->username/edit";
+		$item = new ElggMenuItem('microthemes:profile:edit', elgg_echo('microthemes:profile:edit'), $url);
+		$item->setSection('action');
+		$return[] = $item;
+	}
+	return $return;
+}
