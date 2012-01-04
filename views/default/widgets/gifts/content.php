0c22f0a31f15519fbe63a7edd3c8fd252423058d
Sem <sembrestels@riseup.net>
2012-01-04 21:47:08 +0100

    Fixed widget.

--- views/default/widgets/gifts/content.php
+++ views/default/widgets/gifts/content.php
@@ -8,9 +8,9 @@
 $gifts = gifts_get_registered_gifts();
 
 echo '<ul class="elgg-gallery">';
-foreach ($gifts as $gift_img => $gift_name) {
+foreach ($gifts as $gift_name => $gift_img) {
 	echo '<li class="elgg-item">';
-	echo '<a title="'.$gift_name.'" href=""><img src="'.$gift_img.'" /></a>';
+	echo '<a title="'.elgg_echo("gifts:gift:$gift_name").'" href=""><img src="'.$gift_img.'" /></a>';
 	echo '</li>';	
 }
 echo '</ul>';
