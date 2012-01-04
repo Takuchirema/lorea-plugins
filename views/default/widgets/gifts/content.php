325edee199ed3baed0dbb82fec4714a5f65443ee
Sem <sembrestels@riseup.net>
2012-01-04 23:44:01 +0100

    Added javascript to widget

--- views/default/widgets/gifts/content.php
+++ views/default/widgets/gifts/content.php
@@ -6,11 +6,29 @@
  */
 
 $gifts = gifts_get_registered_gifts();
+$owner = elgg_get_page_owner_entity();
 
 echo '<ul class="elgg-gallery">';
 foreach ($gifts as $gift_name => $gift_img) {
+	$gift_name = elgg_echo("gifts:gift:$gift_name");
+	$give = elgg_echo("gifts:give", array($gift_name, $owner->name));
 	echo '<li class="elgg-item">';
-	echo '<a title="'.elgg_echo("gifts:gift:$gift_name").'" href=""><img src="'.$gift_img.'" /></a>';
+	echo '<a title="'.$give.'" href=""><img class="gift" src="'.$gift_img.'" alt="'.$gift_name.'" /></a>';
 	echo '</li>';	
 }
 echo '</ul>';
+
+?>
+
+<script type ="text/javascript">
+
+$(function(){
+	$('img.gift').mouseover(function(){
+		$('img.gift').stop().fadeTo(1000, 0.3);
+		$(this).stop().fadeTo(1000, 1);
+	}).mouseout(function(){
+		$('img.gift').stop().fadeTo(1000, 1);
+	});
+});
+
+</script>
