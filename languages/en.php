37d3c1ec969048f37c62bc751c64cca7aa8b4c09
Sem <sembrestels@riseup.net>
2012-01-05 00:13:01 +0100

    Added languages file.

---
+++ languages/en.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Elgg gifts plugin language pack
+ *
+ * @package ElggGifts
+ */
+
+$english = array(
+
+	/**
+	 * Menu items and titles
+	 */
+	'gifts' => "Gifts",
+	'gifts:give' => "Give a %s to %s",
+	
+	/**
+	 * Gift names
+	 */
+	'gifts:gift:banana' => 'Banana',
+	'gifts:gift:flower' => 'Flower',
+	'gifts:gift:coal_lorry' => 'Coal lorry',
+	'gifts:gift:badge' => 'Badge',
+	'gifts:gift:seed' => 'Seed',
+	'gifts:gift:heart' => 'Heart',
+	'gifts:gift:organic_carrot' => 'Organic carrot',
+	'gifts:gift:cocktail_molotov' => 'Cocktail Molotov',
+);
+
+add_translation("en", $english);
