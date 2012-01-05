45efb51b7e77cbdcd8335dd1f4f19bb0286cd3f1
Sem <sembrestels@riseup.net>
2012-01-05 07:03:17 +0100

    Fixed gift creation for non admins.

--- actions/gifts/send.php
+++ actions/gifts/send.php
@@ -18,12 +18,12 @@ if(!elgg_instanceof($owner, 'user') || !array_key_exists($gift_type, gifts_get_r
 	forward(REFERER);
 }
 
-if($owner->guid == $user_guid()) {
+if($owner->guid == $user_guid) {
 	register_error(elgg_echo("gifts:yourself"));
 	forward(REFERER);
 }
 
-if(!$owner->isFriendsWith($user_guid())) {
+if(!$owner->isFriendsWith($user_guid)) {
 	register_error(elgg_echo("gifts:nofriends"));
 	forward(REFERER);
 }
@@ -31,16 +31,18 @@ if(!$owner->isFriendsWith($user_guid())) {
 $gift = new ElggObject();
 $gift->subtype = "gift";
 $gift->title = $note;
-$gift->owner_guid = $owner_guid;
-$gift->sender_guid = $user_guid();
+$gift->owner_guid = $owner->guid;
+$gift->sender_guid = $user_guid;
 $gift->gift_type = $gift_type;
 $gift->access_id = ACCESS_PUBLIC;
 
+$ignore_access = elgg_set_ignore_access(true); // To change gift's owner.
 if ($gift->save()) {
 	system_message(elgg_echo("gift:sent", array($owner->name)));
 	add_to_river('river/object/gift/create', 'create', $owner->guid, $gift->guid);
 } else {
 	register_error(elgg_echo("gifts:sendfailed"));
 }
+elgg_set_ignore_access($ignore_access);
 
 forward(REFERER);
