e1611bf0dc34c67339f78d0beb841cfbbf101f17
Sem <sembrestels@riseup.net>
2012-02-13 21:41:22 +0100

    Added manifest and start files.

---
+++ start.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Online plugin
+ * 
+ * This is a rewrite of the Bogdan Nikovskiy's Online plugin,
+ * written in 2009.
+ *
+ */
+
+elgg_register_event_handler('init', 'system', 'online_init');
+
+function online_init() {
+
+	elgg_extend_view('css/elgg', 'online/css');
+}
