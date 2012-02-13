da76c808493808400e5f77b0c04f5c54862a1b6d
Sem <sembrestels@riseup.net>
2012-02-13 22:11:42 +0100

    Fixed bug.

--- views/default/icon/user/default.php
+++ views/default/icon/user/default.php
@@ -79,7 +79,7 @@ $show_menu = $use_hover && (elgg_is_admin_logged_in() || !$user->isBanned());
 
 $is_online = $user->last_action >= time() - 600;
 
-if ($is_online && !$user->isFriend()) {
+if ($is_online && $user->isFriend()) {
 	$icon .= elgg_view_icon('online', 'icon-over');
 }
 
