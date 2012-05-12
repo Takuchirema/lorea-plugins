162a741a8e3c0142ced466e3cc47d309a87b6867
Sem <sembrestels@riseup.net>
2012-05-12 06:17:31 +0200

    Added catalan, basque and galician languages.

---
+++ languages/ca.php
@@ -0,0 +1,18 @@
+<?php
+$ca = array (
+  'suicide:suicide' => '%s s\'ha suïcidat digitalment',
+  'suicide:nocares' => ', a ningú li importa.',
+  'suicide:autist' => ', va passar sense pena ni glòria.',
+  'suicide:lonely' => ', no tenia massa amistats.',
+  'suicide:normal' => '. Rest In Bits.',
+  'suicide:popular' => '. Se\'l trobarà en falta (o no).',
+  'suicide:respected' => ', havia nascut com a líder.',
+  'suicide:godlike' => '. Per què ens has deixat?',
+  'suicide:success' => 'T\'has suïcidat correctament, et trobarem a faltar!',
+  'suicide:fail' => 'Ha passat alguna cosa amb el procés de suïcidi. Siusplau comunica-ho als administradors/es de la xarxa.',
+  'suicide:submit' => 'Suïcidi ',
+  'suicide:confirm' => 'Segur que ho vols fer? no hi ha marxa enrere! Això també esborrarà tots els objectes que et pertanyen, els que siguin de un grup s\'assignaran al/la propietari/a del grup.',
+);
+
+add_translation("ca", $ca);
+
