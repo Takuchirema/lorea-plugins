dcb6d8b3f4a0954573c29330acecade9a347af74
Sem <sembrestels@riseup.net>
2012-05-24 02:58:26 +0200

    Better without a function call.

--- views/default/forms/groups/edit.php
+++ views/default/forms/groups/edit.php
@@ -141,7 +141,7 @@ if (isset($vars['entity'])) {
 if (elgg_get_page_owner_guid()) {
 	echo elgg_view('input/hidden', array(
 		'name' => 'container_guid',
-		'value' => $vars['entity']->getGUID() ?
+		'value' => isset($vars['entity']) ?
 										$vars['entity']->getContainerGUID() :
 										elgg_get_page_owner_guid(),
 	));
