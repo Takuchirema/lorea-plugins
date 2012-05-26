115c196c47f37ba7e3af33661c66a8c6f60ea10e
Sem <sembrestels@riseup.net>
2012-05-26 09:58:15 +0200

    Upgraded start to Elgg 1.8 style.

--- start.php
+++ start.php
@@ -1,75 +1,118 @@
 <?php
 /**
-         * Elgg powered plugin
-         * 
-         * @package
-         * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-         * @author lorea
-         * @copyright lorea
-         * @link http://lorea.cc
-         */
+ * Elgg Microthemes
+ *
+ * @package ElggMicrothemes
+ */
+ 
+elgg_register_event_handler('init', 'system', 'microthemes_init');
 
-	function microthemes_tasksicon_hook($hook, $entity_type, $returnvalue, $params) {
-		if ($hook == 'entity:icon:url') {
-			$entity = $params['entity'];
-			if ($entity->getSubtype() == 'microthemes') {
-				return $entity->bgurl;
+/**
+ * Initialize the microthemes plugin.
+ *
+ */
+function microthemes_init(){
+	
+	// Register a page handler, so we can have nice URLs
+	elgg_register_page_handler('microthemes', 'microthemes_page_handler');
+	
+	// Register some actions
+	$action_base = elgg_get_plugins_path() . 'pages/actions/pages';
+	elgg_register_action("microthemes/delete", "$action_base/delete.php");
+	elgg_register_action("microthemes/clear", "$actions_base/clear.php");
+	elgg_register_action("microthemes/edit", "$actions_base/edit.php");
+	elgg_register_action("microthemes/choose", "$actions_base/choose.php");
+	
+	elgg_register_plugin_hook_handler('entity:icon:url', 'object', 'microthemes_icon_url_override');
+	elgg_register_plugin_hook_handler('register', 'menu:user_hover', 'microthemes_user_hover_menu');
+	
+	elgg_register_event_handler('pagesetup', 'system', 'microthemes_pagesetup');
+	
+	elgg_register_menu_item('page', array(
+		'name' => 'choose_profile_microtheme',
+		'href' => "microthemes/owner/" . elgg_get_page_owner_entity()->username,
+		'text' => elgg_echo('microthemes:profile:edit'),
+		'contexts' => array('profile_edit'),
+	));
+	
+	elgg_extend_view("page/elements/head", "microthemes/metatags");
+}
+
+/**
+ * Dispatcher for microthemes.
+ * URLs take the form of
+ *  @todo All microthemes:        microthemes/all
+ *  @todo User's microtheme:      microthemes/owner/<username>
+ *  @todo Friends' microthemes:   microthemes/friends/<username>
+ *  View CSS:               microthemes/css/<guid>/
+ *  New microtheme:         microthemes/add/<guid> (container: user, group)
+ *  Edit microtheme:        microthemes/edit/<guid>
+ *  Group microthemes:      microthemes/group/<guid>/all
+ *
+ * @param array $page
+ * @return bool
+ */
+function microthemes_page_handler($page) {
+	elgg_push_context('microthemes');
+	$page_base = elgg_get_plugins_path() . "pages/microthemes";
+	switch ($page[0]) {
+		case 'css':
+			include("$page_base/css.php");
+			break;
+		case 'new':
+		case 'edit':
+			set_input('object_guid', $page[1]);
+			include("$page_base/edit.php");
+			break;
+		case 'owner':
+		case 'group':
+			set_input('assign_to', $page[1]);
+			include("$page_base/view.php");
+			break;
+		default:
+			if (!get_input('assign_to')) {
+				set_input('assign_to', elgg_get_logged_in_user_guid());
 			}
-		}
-		return $returnvalue;
+			include("$page_base/view.php");
+			break;
 	}
+	return true;
+}
 
-	function microthemes_pagesetup() {
-		global $CONFIG;
-		$page_owner = page_owner_entity();
-		if ($page_owner instanceof ElggGroup && get_context() == "groups" && $page_owner->canEdit()) {
-			add_submenu_item(elgg_echo("microthemes:groupthemes"), 
-					$CONFIG->wwwroot . "pg/microthemes/groupview/".$page_owner->getGUID());
-		}
-		if (get_context() == 'settings') {
-			add_submenu_item(elgg_echo('microthemes:userthemes'), $CONFIG->wwwroot . "pg/microthemes/view");
-		}
+
+function microthemes_icon_url_override($hook, $entity_type, $returnvalue, $params) {
+	$entity = $params['entity'];
+	if ($entity->getSubtype() == 'microthemes') {
+		return $entity->bgurl;
 	}
+	return $returnvalue;
+}
 
-	function microthemes_page_handler($page) {
-		global $CONFIG;
-		set_context('microthemes');
-		switch ($page[0]) {
-			case 'css':
-				include($CONFIG->pluginspath.'microthemes/css.php');
-				break;
-			case 'edit':
-				if ($page[1]) {
-					set_input('object_guid', $page[1]);
-				}
-				include($CONFIG->pluginspath.'microthemes/edit.php');
-				break;
-			case 'groupview':
-				set_input('assign_to', $page[1]);
-				include($CONFIG->pluginspath.'microthemes/view.php');
-				break;
-			default:
-				if (!get_input('assign_to'))
-					set_input('assign_to', get_loggedin_userid());
-				include($CONFIG->pluginspath.'microthemes/view.php');
-				break;
-		}
+function microthemes_pagesetup() {
+	$owner = elgg_get_page_owner_entity();
+	if ($owner && $owner->canEdit()) {
+		elgg_register_menu_item('page', array(
+			'name' => 'choose_profile_microtheme',
+			'href' => "microthemes/owner/" . $owner->username,
+			'text' => elgg_echo('microthemes:profile:edit'),
+			'contexts' => array('profile_edit'),
+		));
+		elgg_register_menu_item('page', array(
+			'name' => 'microthemes',
+			'text' => elgg_echo('microthemes:group:edit'),
+			'href' => "microthemes/group/" . $owner->guid,
+			'context' => array('groups'),
+		));
 	}
+}
 
- 	function microthemes_init(){
-			global $CONFIG;
-			register_action("microthemes/delete",false, $CONFIG->pluginspath . "microthemes/actions/microthemes/delete.php");
-			register_action("microthemes/clear",false, $CONFIG->pluginspath . "microthemes/actions/microthemes/clear.php");
-			register_action("microthemes/edit",false, $CONFIG->pluginspath . "microthemes/actions/microthemes/edit.php");
-			register_action("microthemes/choose",false, $CONFIG->pluginspath . "microthemes/actions/microthemes/choose.php");
-			register_plugin_hook('entity:icon:url', 'object', 'microthemes_tasksicon_hook');
-			register_page_handler('microthemes','microthemes_page_handler');
-			register_elgg_event_handler('pagesetup','system','microthemes_pagesetup');
-
-                        elgg_extend_view("metatags", "microthemes/metatags");
-			//elgg_extend_view('profile/menu/linksownpage','microthemes/profilemenu');
+function microthemes_user_hover_menu($hook, $type, $return, $params) {
+	$user = $params['entity'];
+	if ($user->canEdit()) {
+		$url = "microthemes/owner/$user->username";
+		$item = new ElggMenuItem('microthemes:profile:edit', elgg_echo('microthemes:profile:edit'), $url);
+		$item->setSection('action');
+		$return[] = $item;
 	}
-
-register_elgg_event_handler('init','system','microthemes_init');
-
-?>
+	return $return;
+}
