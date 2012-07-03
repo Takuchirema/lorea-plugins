e4c89ab9bcc44c8521a128fa4457e841881a5ead
Sem <sembrestels@riseup.net>
2012-07-03 03:15:12 +0200

    Added CSS.

--- pages/microthemes/css.php
+++ pages/microthemes/css.php
@@ -1,119 +1,27 @@
 <?php
-	global $CONFIG;
-	header("Content-type: text/css");
-	$guid = get_input('guid');
-	$owner = get_entity($guid);
-	if ($guid && $owner && $owner->microtheme) {
-		$microtheme = get_entity($owner->microtheme);
-	$bgopts = 'top';
-	if ($microtheme->bg_alignment)
-		$bgopts .= ' '.$microtheme->bg_alignment;
-	else
-		$bgopts .= ' left';
-	if ($microtheme->repeatx && $microtheme->repeaty) {
-		// nothing is needed
-	}
-	else {
-		if ($microtheme->repeatx)
-			$bgopts .= ' repeat-x';
-		if ($microtheme->repeaty)
-			$bgopts .= ' repeat-y';
-		if (!$microtheme->repeatx && !$microtheme->repeaty)
-			$bgopts .= ' no-repeat';
-	}
-	$height = 120;
-	if ($microtheme->height) {
-		$height = $microtheme->height;
-	}
-	$hide_sitename = false;
-	if ($microtheme->hidesitename)
-		$hide_sitename = true;
-	$translucid_page = false;
-	if ($microtheme->translucid_page)
-		$translucid_page = true;
-
+header("Content-type: text/css");
 
 ?>
 
-.colorSelector {
-	position: relative;
-	width: 36px;
-	height: 36px;
-	background: url(<?php echo $CONFIG->wwwroot; ?>mod/microthemes/vendors/images/select.png);
-}
-.colorSelector div {
-	position: absolute;
-	top: 3px;
-	left: 3px;
-	width: 30px;
-	height: 30px;
-	background: url(<?php echo $CONFIG->wwwroot; ?>mod/microthemes/vendors/images/select.png) center;
+.elgg-page {
+	background-color: #330;
+	background-image: url("http://apod.nasa.gov/apod/image/0210/m31_gendler_big.jpg");
+	background-attachment: fixed;
 }
 
-body, #page_container {
-	background-color: <?php echo $microtheme->bg_color; ?>;
-	background: <?php echo $microtheme->bg_color; ?> url(<?php echo $microtheme->bgurl; ?>) <?php echo $bgopts; ?>;
-
+.elgg-page-header {
+	background: none;
 }
 
-
-
-
-<?php
-	if ($translucid_page) {
-?>
-#layout_canvas {
-	background: transparent;
-	background-image: url(<?php echo $CONFIG->wwwroot; ?>mod/microthemes/graphics/contentbg.png);
-	border-color: #cccccc;
-	border-width-rtl-source: physical;
-	border-width: 1px;
-	border-style: solid;
-	padding: 15px;
+.elgg-page-header ul, .elgg-page-body {
+	opacity: 0.94;
 }
 
-<?php
-	}
-?>
-#layout_header {
-	height: <?php echo $height; ?>px;
-	background: transparent;
+.elgg-page-footer > .elgg-inner {
+	background-color: #000;
 }
 
-a:link.mainmenu_img:link, a.mainmenu_img:visited {
-	background-color: <?php echo $microtheme->topbar_color; ?> ! important;
-	background: <?php echo $microtheme->topbar_color; ?> ! important;
+.elgg-page-footer, .elgg-page-footer a, .elgg-page-footer a:hover, .elgg-page-footer h2,
+.elgg-menu-footer > li, .elgg-menu-footer > li > a {
+	color: #fff;
 }
-
-<?php
-	if ($hide_sitename) {
-?>
-/* Hide sitename */
-#wrapper_header h1 {
-	display: none;
-}
-#wrapper_header h1 a {
-	display: none;
-}
-<?php
-	}
-?>
-
-/* Hide header image */
-#wrapper_header img {
-	display: none;
-}
-
-/* Change the topbar color */
-#elgg_topbar {
-
-	background: <?php echo $microtheme->topbar_color; ?> repeat-x top left;
-        border-bottom-color: <?php echo $microtheme->bg_color; ?>;
-
-}
-/*#page_container {
-	background-color: <?php echo $microtheme->bg_color; ?>;
-}*/
-<?php
-	}
-?>
--- views/default/microthemes/metatags.php
+++ views/default/microthemes/metatags.php
@@ -1,26 +1,6 @@
-<?php
-	global $CONFIG;
-	$page_owner = page_owner_entity();
-	$owner = null;
-	if ((($page_owner instanceof ElggUser && (get_context() == "profile" || get_context() == "microthemes" || get_context() == "dashboard")) || 
-	    ($page_owner instanceof ElggGroup)) && $page_owner->microtheme && get_entity($page_owner->microtheme)) {
-		$owner = $page_owner;
-	}
-	else if (get_context() == "microthemes") {
-		$owner = get_entity(get_input("assign_to"));
-		if (!$owner) {
-			$user = get_loggedin_user();
-			if ($user->microtheme && get_entity($user->microtheme))
-				$owner = $user;
-		}
-	}
-	if (!$owner) {
-		$site = get_site_by_url($CONFIG->wwwroot);
-		$owner = $site;
-	}
-	if ($owner && $owner->microtheme && get_entity($owner->microtheme)) {
-?>
-	<link rel="stylesheet" href="<?php echo $vars['url']; ?>pg/microthemes/css&guid=<?php echo $owner->getGUID(); ?>" type="text/css" />
-<?php
-	}
-?>
+<?php	
+$page_owner = elgg_get_page_owner_entity();
+
+if ($page_owner) {// && $page_owner->microtheme && get_entity($page_owner->microtheme)) {
+	echo "<link rel=\"stylesheet\" href=\"{$vars['url']}microthemes/css/{$page_owner->username}\" type=\"text/css\" />";
+}
