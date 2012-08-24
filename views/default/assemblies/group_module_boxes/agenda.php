509b6838ce97a7b252cab412215bdfb41600a264
Pablo Martin <caedes@sindominio.net>
2012-08-24 04:56:51 +0000

    fill in agenda points in group widget.

--- views/default/assemblies/group_module_boxes/agenda.php
+++ views/default/assemblies/group_module_boxes/agenda.php
@@ -1,4 +1,5 @@
 <?php
+elgg_load_library('elgg:crud');
 
 echo "<b>".elgg_echo("assemblies:agenda")."</b>";
 
@@ -7,7 +8,15 @@ $group = elgg_get_page_owner_entity();
 // Get next assembly
 $assembly = $vars['next_assembly'];
 if (!empty($assembly)) {
-	echo "<p>".date("m.d.y", $assembly->date)."</p>";
+	$children = crud_get_children($assembly);
+	echo "<ul>";
+	foreach ($children as $child) {
+		echo "<li>".elgg_view('output/url', array(
+        		'href' => "agenda_point/view/$child->guid",
+		        'text' => $child->title,
+			))."</li>";
+	}
+	echo "</ul>";
 } else {
 	echo elgg_echo("assemblies:none");
 }
