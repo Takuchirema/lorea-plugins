95454c96c014aa0dd902fd0b63449b296629d740
Pablo Martin <caedes@sindominio.net>
2012-08-24 04:14:35 +0000

    fix formatting on assembly info box.

--- views/default/assemblies/group_module_boxes/info.php
+++ views/default/assemblies/group_module_boxes/info.php
@@ -2,15 +2,11 @@
 
 $assembly = $vars['next_assembly'];
 if (!empty($assembly)) {
-	echo "<p>";
 	echo "<b>".elgg_echo("assemblies:info:when")."</b>";
-	echo "</p>";
 	echo "<p>";
 	echo date("d.m.y", $assembly->date);
 	echo "</p>";
-	echo "<p>";
 	echo "<b>".elgg_echo("assemblies:info:where")."</b>";
-	echo "</p>";
 } else {
 	echo elgg_echo("assemblies:none");
 }
