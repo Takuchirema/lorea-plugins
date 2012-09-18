9d03f5be828eb5a15e72bcd37dd09a74997eaeaf
Pablo Martin <caedes@sindominio.net>
2012-09-18 08:23:35 +0000

    add missing language strings.

--- languages/en.php
+++ languages/en.php
@@ -2,6 +2,11 @@
 $english = array(
 	"push:hub:label" => 'Hub URL',
 	"push:hub:details" => 'If you don\'t already have your own hub, http://pubsubhubbub.appspot.com/ is a free community hub that anyone can use',
+	"push:input:atomurl" => 'Input an ostatus url to subscribe to',
+	"push:state:subscribe failed" => 'subscribe failed',
+	"push:delete:success" => "Subscription deleted successfully",
+	"push:delete:failed" => "Failed to delete the subscription",
+	"admin:administer_utilities:push" => 'Pubsubhub subscriptions',
 );
 
 add_translation("en",$english);
