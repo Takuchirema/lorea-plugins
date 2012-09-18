12441a025c65822d453bf62a5da1d2ad4497470c
Pablo Martin <caedes@sindominio.net>
2012-09-18 08:19:18 +0000

    add a form for push subscription.

---
+++ views/default/forms/push/subscribe.php
@@ -0,0 +1,7 @@
+<?php
+	$body = "<p><label>".elgg_echo("push:input:atomurl")."</label><br />";
+	$body .= elgg_view("input/text", array('value'=>'', 'internalname'=>'atom_url'));
+	$body .= elgg_view("input/submit", array('value'=>'Submit', 'internalname'=>'submit'))."</p>";
+	//echo elgg_view("input/form", array('body'=>$body, 'action'=>$vars['url'].'action/pshb/subscribe'));
+	echo $body;
+?>
