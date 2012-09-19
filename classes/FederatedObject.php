caab47fc6265c4f6b827e79775be04ae99a33ac2
Pablo Martin <caedes@sindominio.net>
2012-09-19 07:02:03 +0000

    error logs

--- classes/FederatedObject.php
+++ classes/FederatedObject.php
@@ -21,6 +21,7 @@ class FederatedObject {
 		global  $FEDERATED_CONSTRUCTORS;
 		$type = $params['type'];
 		$entity = FederatedObject::find($params['id']);
+		error_log("CREATE:".$FEDERATED_CONSTRUCTORS[$type].":".$params['id']);
 		return call_user_func($FEDERATED_CONSTRUCTORS[$type], $params, $entity);
 	}
 	public static function register_constructor($type, $callback) {
