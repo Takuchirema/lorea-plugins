6ebe61a76a291028271ba39b97c1c1526eabcb98
Pablo Martin <caedes@sindominio.net>
2012-09-19 05:23:34 +0000

    initial commit

---
+++ start.php
@@ -0,0 +1,45 @@
+<?php
+
+// General callbacks
+function federated_objects_notification($hook, $type, $return, $params) {
+	// input parameters
+	$entry = $params['entry'];
+	$subscriber = $params['subscriber'];
+	$salmon_link = $params['salmon_link'];
+
+	$federated = new FederatedObject();
+	$federated->load($entry);
+
+	// parse verb
+	$verb = $federated->getVerb();
+
+	// parse object type
+	$object_type = $federated->getObjectType();
+
+	$target = $federated->getObject();
+
+	// output
+	$params = array('notification' => $federated,
+			'subscriber' => $subscriber,
+			'salmon_link' => $salmon_link,
+			'entry' => $entry);
+	trigger_plugin_hook('federated_objects:'.$verb, $object_type, $params);
+}
+
+// Specific callbacks for river actions
+function federated_objects_action_post_article($hook, $type, $return, $params) {
+	$federated = $params['notification'];
+	error_log("action: $hook $type");
+}
+
+function federated_objects_init() {
+	#elgg_register_library('elgg:push', elgg_get_plugins_path() . 'elgg-push/lib/push.php');
+
+	elgg_register_plugin_hook_handler('push:notification', 'atom', 'federated_objects_notification');
+
+	elgg_register_plugin_hook_handler('federated_objects:post', 'article', 'federated_objects_action_post_article');
+	elgg_register_plugin_hook_handler('federated_objects:post', 'bookmark', 'federated_objects_action_post_article');
+	elgg_register_plugin_hook_handler('federated_objects:post', 'note', 'federated_objects_action_post_article');
+}
+
+elgg_register_event_handler('init', 'system', 'federated_objects_init');
