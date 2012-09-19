36d8121de00cc0beb84168138fbb62a7c9305b86
Pablo Martin <caedes@sindominio.net>
2012-09-19 08:14:06 +0000

    documenting.

--- start.php
+++ start.php
@@ -1,15 +1,17 @@
 <?php
 
 function federated_objects_init() {
-	#elgg_register_library('elgg:push', elgg_get_plugins_path() . 'elgg-push/lib/push.php');
 
+	// callback for incoming items for now coming from pubsubhub
 	elgg_register_plugin_hook_handler('push:notification', 'atom', array('FederatedNotification', 'notification'));
 
-	// callbacks for specific actions
+	// callbacks for specific actions, implement procedures to manage object - verb combinations
 	elgg_register_plugin_hook_handler('federated_objects:post', 'article', array('FederatedNotification', 'postLogger'));
 	elgg_register_plugin_hook_handler('federated_objects:post', 'bookmark', array('FederatedNotification', 'postLogger'));
 	elgg_register_plugin_hook_handler('federated_objects:post', 'note', array('FederatedNotification', 'postObjectCreator'));
 	elgg_register_plugin_hook_handler('federated_objects:join', 'group', array('FederatedNotification', 'postLogger'));
+
+	// object constructors, plugins can register their own to support new data types
 	FederatedObject::register_constructor('person', array('FederatedObject', 'create_person'));
 	FederatedObject::register_constructor('note', array('FederatedObject', 'create_note'));
 }
