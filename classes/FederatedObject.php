d82ee494cbed5fa69613b5802997cdedd4e03e50
Pablo Martin <caedes@sindominio.net>
2012-09-19 08:01:53 +0000

    move object creating code to FederatedObject class

--- classes/FederatedObject.php
+++ classes/FederatedObject.php
@@ -17,6 +17,7 @@ class FederatedObject {
                         return $entities[0];
                 }
 	}
+
 	public static function create($params) {
 		global  $FEDERATED_CONSTRUCTORS;
 		$type = $params['type'];
@@ -24,8 +25,83 @@ class FederatedObject {
 		error_log("CREATE:".$FEDERATED_CONSTRUCTORS[$type].":".$params['id']);
 		return call_user_func($FEDERATED_CONSTRUCTORS[$type], $params, $entity);
 	}
+
 	public static function register_constructor($type, $callback) {
 		global  $FEDERATED_CONSTRUCTORS;
 		$FEDERATED_CONSTRUCTORS[$type] = $callback;
 	}
+
+	public static function randomString($length)
+	{
+	    // Generate random 32 charecter string
+	    $string = md5(time());
+
+	    // Position Limiting
+	    $highest_startpoint = 32-$length;
+
+	    // Take a random starting point in the randomly
+	    // Generated String, not going any higher then $highest_startpoint
+	    $randomString = substr($string,rand(0,$highest_startpoint),$length);
+
+	    return $randomString;
+
+	}
+
+	public static function create_person($params, $entity) {
+		if ($entity) {
+			error_log("federated_objects_create_person:exists!");
+		}
+		else {
+			error_log("federated_objects_create_person:doesnt exists!". $params['id']);
+			$access = elgg_set_ignore_access(true);
+			$entity = new ElggUser();
+			$entity->owner_guid = 0;
+			$entity->container_guid = 0;
+			$entity->subtype = 'ostatus';
+			$entity->username = FederatedObject::randomString(8);
+			$entity->save();
+			$entity->username = 'ostatus_'.$entity->getGUID();
+			$entity->name = $params['name'];
+			$entity->access_id = ACCESS_PUBLIC;
+			$entity->atom_id = $params['id'];
+			$entity->foreign = true;
+			$entity->save();
+			elgg_set_ignore_access($access);
+		}
+		return $entity;
+	}
+
+	public static function create_note($params, $entity) {
+		$owner = $params['owner_entity'];
+		$entry = $params['entry'];
+		$access_id = ACCESS_PUBLIC;
+		$method = 'ostatus';
+
+		$body = @current($entry->xpath("activity:object/atom:content"));
+		$body = elgg_strip_tags($body);
+		if (empty($body)) {
+			$body = $entry->xpath("atom:content");
+			if (is_array($body))
+				$body = @current($body);
+			if ($body)
+				$body = $body->asXML();
+		}
+
+		if ($entity) {
+			$note = $entity;
+		}
+		else {
+			$access = elgg_set_ignore_access(true);
+
+			$guid = thewire_save_post($body, $owner->getGUID(), $access_id, $parent_guid, $method);
+			$note = get_entity($guid);
+			$note->atom_id = $params['id'];
+			$note->foreign = true;
+
+			elgg_set_ignore_access($access);
+		}
+		return $note;
+}
+
+
 }
--- start.php
+++ start.php
@@ -49,79 +49,6 @@ function federated_objects_action_post_note($hook, $type, $return, $params) {
 	error_log("note: $hook $type");
 }
 
-function fo_randomString($length)
-{
-    // Generate random 32 charecter string
-    $string = md5(time());
-
-    // Position Limiting
-    $highest_startpoint = 32-$length;
-
-    // Take a random starting point in the randomly
-    // Generated String, not going any higher then $highest_startpoint
-    $randomString = substr($string,rand(0,$highest_startpoint),$length);
-
-    return $randomString;
-
-}
-
-function federated_objects_create_person($params, $entity) {
-	if ($entity) {
-		error_log("federated_objects_create_person:exists!");
-	}
-	else {
-		error_log("federated_objects_create_person:doesnt exists!". $params['id']);
-		$access = elgg_set_ignore_access(true);
-		$entity = new ElggUser();
-		$entity->owner_guid = 0;
-		$entity->container_guid = 0;
-		$entity->subtype = 'ostatus';
-		$entity->username = fo_randomString(8);
-		$entity->save();
-		$entity->username = 'ostatus_'.$entity->getGUID();
-		$entity->name = $params['name'];
-                $entity->access_id = ACCESS_PUBLIC;
-		$entity->atom_id = $params['id'];
-		$entity->foreign = true;
-		$entity->save();
-		elgg_set_ignore_access($access);
-	}
-	return $entity;
-}
-
-function federated_objects_create_note($params, $entity) {
-	$owner = $params['owner_entity'];
-	$entry = $params['entry'];
-	$access_id = ACCESS_PUBLIC;
-	$method = 'ostatus';
-
-	$body = @current($entry->xpath("activity:object/atom:content"));
-	$body = elgg_strip_tags($body);
-	if (empty($body)) {
-		$body = $entry->xpath("atom:content");
-		if (is_array($body))
-			$body = @current($body);
-		if ($body)
-			$body = $body->asXML();
-	}
-
-	if ($entity) {
-		$note = $entity;
-	}
-	else {
-		$access = elgg_set_ignore_access(true);
-
-		$guid = thewire_save_post($body, $owner->getGUID(), $access_id, $parent_guid, $method);
-		$note = get_entity($guid);
-		$note->atom_id = $params['id'];
-		$note->foreign = true;
-
-		elgg_set_ignore_access($access);
-	}
-	return $note;
-}
-
-
 function federated_objects_init() {
 	#elgg_register_library('elgg:push', elgg_get_plugins_path() . 'elgg-push/lib/push.php');
 
@@ -132,8 +59,8 @@ function federated_objects_init() {
 	elgg_register_plugin_hook_handler('federated_objects:post', 'bookmark', 'federated_objects_action_post_article');
 	elgg_register_plugin_hook_handler('federated_objects:post', 'note', 'federated_objects_action_post_note');
 	elgg_register_plugin_hook_handler('federated_objects:join', 'group', 'federated_objects_action_post_article');
-	FederatedObject::register_constructor('person', 'federated_objects_create_person');
-	FederatedObject::register_constructor('note', 'federated_objects_create_note');
+	FederatedObject::register_constructor('person', array('FederatedObject', 'create_person'));
+	FederatedObject::register_constructor('note', array('FederatedObject', 'create_note'));
 }
 
 elgg_register_event_handler('init', 'system', 'federated_objects_init');
