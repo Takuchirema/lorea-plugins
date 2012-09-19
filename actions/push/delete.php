13c868642922efbfca87fdd4ec942513c6f935a1
Pablo Martin <caedes@sindominio.net>
2012-09-19 02:39:04 +0000

    add unsubscription when deleting a subscription.

--- actions/push/delete.php
+++ actions/push/delete.php
@@ -1,12 +1,14 @@
 <?php
         admin_gatekeeper();
 
+	elgg_load_library('elgg:push');
+
 	$guid = get_input('guid');
 	if ($entity = get_entity($guid)) {
 		
 		if ($entity->canEdit() || isadminloggedin()) {
 			
-			if ($entity->delete()) {
+			if (push_unsubscribeto($entity->topic) && $entity->delete()) {
 				
 				system_message(elgg_echo("push:delete:success"));
 				forward("admin/administer_utilities/push");				
--- lib/push.php
+++ lib/push.php
@@ -11,13 +11,25 @@ function push_subscribeto($url) {
 }
 
 /*
- * push_create_subscription
+ * push_unsubscribeto
  *
- * create the given subscription
+ * unsubscribe to given url
  */
-function push_create_subscription($subscription_id, $url) {
-	$site_url = elgg_get_site_url();
+function push_unsubscribeto($url) {
+        $subscription_id = sha1($url . get_site_secret());
+        $sub = PuSHSubscriber::instance('elgg_subs', $subscription_id, 'ElggPuSHSubscription', new ElggPuSHEnvironment());
+        if ($sub->unsubscribe($url,
+                        $site_url . "push/". $subscription_id) != false) {
+		push_delete_subscriptions($subscription_id);
+		return true;
+	}
+	return false;
+}
 
+/*
+ * Delete all subscriptions for given subscription id
+ */
+function push_delete_subscriptions($subscription_id) {
         $metadata_pairs = array('subscriber_id' => $subscription_id);
         $options = array('metadata_name_value_pairs' => $metadata_pairs,
 			 'types' => 'object',
@@ -30,6 +42,17 @@ function push_create_subscription($subscription_id, $url) {
                 	$entity->delete();
 	        }
         }
+}
+
+/*
+ * push_create_subscription
+ *
+ * create the given subscription
+ */
+function push_create_subscription($subscription_id, $url) {
+	$site_url = elgg_get_site_url();
+
+	push_delete_subscriptions($subscription_id);
 
         $sub = PuSHSubscriber::instance('elgg_subs', $subscription_id, 'ElggPuSHSubscription', new ElggPuSHEnvironment());
         if ($xml = $sub->subscribe($url,
@@ -42,7 +65,6 @@ function push_create_subscription($subscription_id, $url) {
                 return true;
         }
         else {
-                error_log("created subscription failed");
                 return false;
         }
 }
