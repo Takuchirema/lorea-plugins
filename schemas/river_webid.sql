1e4d7e0f269dc62e51ce012f78d41332eec81655
Pablo Martin <caedes@sindominio.net>
2012-09-24 02:32:22 +0000

    add provenance to river webid mapping

--- schemas/river_webid.sql
+++ schemas/river_webid.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `prefix_river_atomid_mapping` (
   `river_id` int(11) NOT NULL,
   `atom_id` text NOT NULL,
+  `provenance` text,
   PRIMARY KEY (`river_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
