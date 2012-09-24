63cde7d73ed491de08010b93a8734a9f3f0c837c
Pablo Martin <caedes@sindominio.net>
2012-09-24 06:05:39 +0000

    add languages file.

---
+++ languages/en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Federated Objects English language file.
+ *
+ */
+
+$english = array(
+	'federated_objects:federated' => 'Federated',
+	'federated_objects:provenance' => 'Provenance',
+);
+
+add_translation('en', $english);
