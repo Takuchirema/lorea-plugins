678c3053b114d59ad119a5b64f8274fa50d1e90f
Pablo Martin <caedes@sindominio.net>
2012-10-02 09:09:19 +0000

    add namespaces for entry shell for salmon.

--- classes/SalmonProtocol.php
+++ classes/SalmonProtocol.php
@@ -29,8 +29,8 @@ class SalmonProtocol {
 				    false,
 				    false,
 				    'atom');
-		$update = "<entry xmlns='http://www.w3.org/2005/Atom' xmlns:thr='http://purl.org/syndication/thread/1.0' xmlns:georss='http://www.georss.org/georss' xmlns:activity='http://activitystrea.ms/spec/1.0/' xmlns:media='http://purl.org/syndication/atommedia'>$update</entry>";
 		elgg_set_viewtype($viewtype);
+		$update = "<entry xmlns='http://www.w3.org/2005/Atom' xmlns:thr='http://purl.org/syndication/thread/1.0' xmlns:georss='http://www.georss.org/georss' xmlns:activity='http://activitystrea.ms/spec/1.0/' xmlns:media='http://purl.org/syndication/atommedia' xmlns:poco='http://portablecontacts.net/spec/1.0' xmlns:context='http://activitystrea.ms/context/" . elgg_view('extensions/xmlns') .">$update</entry>";
 		SalmonProtocol::postEnvelope($salmon_link, $update, $subject);
 	}
 
