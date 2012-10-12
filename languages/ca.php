52cd7cce2bfca5bc33f1593870a8f3468a7a3fa4
Sem <sembrestels@riseup.net>
2012-10-12 17:57:50 +0200

    Added languages.

---
+++ languages/ca.php
@@ -0,0 +1,7 @@
+<?php
+$language = array (
+  'groups:alias' => 'Àlies del grup',
+  'groups:alias:invalid' => 'Hi ha caràcters invàlids',
+  'groups:alias:already_taken' => 'Aquest àlies de grup ja apunta a un altre grup',
+);
+add_translation("ca", $language);
