4d458cd72d83b9034e81dee9035bed8952b965e9
Sem <sembrestels@riseup.net>
2012-10-12 17:56:56 +0200

    Added catalan languages.

---
+++ languages/ca.php
@@ -0,0 +1,20 @@
+<?php
+$language = array (
+  'gifts' => 'Regals',
+  'gifts:give' => 'Dóna %s a %s',
+  'gifts:enclose' => 'Dóna %s amb una nota',
+  'gifts:sent' => 'Has enviat un regal a %s',
+  'gifts:sendfailed' => 'Ho sento, el teu regal s\'ha perdut pel viatge',
+  'gifts:yourself' => 'Enviar-se un regal no és l\'actitud!',
+  'gifts:nofriends' => 'Només pots enviar regals a les teves amigues',
+  'river:create:object:gift' => '%s ha rebut %s de %s',
+  'gifts:gift:banana' => 'un plàtan',
+  'gifts:gift:flower' => 'una flor',
+  'gifts:gift:coal_lorry' => 'un camió amb carbó',
+  'gifts:gift:badge' => 'una medalla',
+  'gifts:gift:seed' => 'una llavor',
+  'gifts:gift:heart' => 'el teu cor',
+  'gifts:gift:organic_carrot' => 'una pastanaga orgànica',
+  'gifts:gift:molotov_cocktail' => 'un còctel Molotov',
+);
+add_translation("ca", $language);
