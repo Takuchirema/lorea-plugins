8e505784f427e1ae7ed0e3c818157b95989bd6f9
occam <occam@lorea.org>
2012-10-20 01:15:40 +0000

    fix umlaut problems. make sure to use utf8

--- languages/de.php
+++ languages/de.php
@@ -7,14 +7,14 @@ $german = array(
     'favorites:items' => 'Favoriten',
     'favorites:menu' => 'Favoriten',
     'favorites:groups' => 'Gruppen',
-    'favorites:add' => 'Zu den Favoriten hinzuf�gen',
+    'favorites:add' => 'Zu den Favoriten hinzufügen',
     'favorites:remove' => 'Aus den Favoriten entfernen',
     'favorites:widget:title' => 'Favoriten',
     'favorites:widget:description' => 'Auflistung der aktuellen Favoriten',
     'favorites:removed' => 'Das Element wurde aus den Favoriten entfernt',
     'favorites:removefailed' => 'Das Element konnte nicht aus den Favoriten enfernt werden',
-    'favorites:added' => 'Das Element wurde zu den Favoriten hinzugef�gt',
-    'favorites:addfailed' => 'Das Element konnte nicht zu den Favoriten hinzugef�gt werden',
+    'favorites:added' => 'Das Element wurde zu den Favoriten hinzugefügt',
+    'favorites:addfailed' => 'Das Element konnte nicht zu den Favoriten hinzugefügt werden',
     'favorites:numbertodisplay' => 'Anzahl der Favoriten zur Anzeige'
 );
 	
