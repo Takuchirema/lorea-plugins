97f52c9b4e3da595bb20aa3940e2d883b8716004
Sem <sembrestels@riseup.net>
2012-10-23 08:09:41 +0200

    Added more translations.

--- languages/ca.php
+++ languages/ca.php
@@ -1,7 +1,9 @@
 <?php
 $language = array (
-  'groups:alias' => 'Àlies del grup',
-  'groups:alias:invalid' => 'Hi ha caràcters invàlids',
-  'groups:alias:already_taken' => 'Aquest àlies de grup ja apunta a un altre grup',
+	'groups:alias' => 'Àlies del grup',
+	'groups:alias:registration:usernametooshort' => "L'àlies del grup ha de tenir menys de %u caràcters.",
+	'groups:alias:registration:usernametoolong' => "L'àlies del grup és massa llarg. Com a màxim pot tenir %u caràcters.",
+	'groups:alias:registration:invalidchars' => "Perdona, l'àlies del grup conté el caràcter %s que és invàlid. Els caràcters sigüents són invàlids: %s",
+	'groups:alias:already_taken' => 'Aquest àlies de grup ja apunta a un altre grup',
 );
 add_translation("ca", $language);
--- languages/es.php
+++ languages/es.php
@@ -1,8 +1,10 @@
 <?php
 $es = array (
-  'groups:alias' => 'Alias del grupo',
-  'groups:alias:invalid' => 'Hay caracteres no válidos',
-  'groups:alias:already_token' => 'El alias del grupo ya está cogido',
+	'groups:alias' => 'Alias del grupo',
+	'groups:alias:registration:usernametooshort' => 'El alias del grupo debe tener menos de %u carácteres.',
+	'groups:alias:registration:usernametoolong' => "El alias del grupo es demasiado largo. Como máximo puede tener %u carácteres.",
+	'groups:alias:registration:invalidchars' => "Perdona, el alias del grupo contiene el carácter %s que es inválido. Los carácteres siguientes son inválidos: %s",
+	'groups:alias:already_token' => 'El alias del grupo ya está cogido',
 );
 
 add_translation("es", $es);
