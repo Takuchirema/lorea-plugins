2c5a8a417cceb2cd1927df5500ff4ca678de27f8
Pablo Martin <caedes@sindominio.net>
2012-10-31 00:08:56 +0000

    change back naming to work elements in spanish translation as favorites is taken :-P

--- languages/es.php
+++ languages/es.php
@@ -4,19 +4,19 @@
  */
 
 $es = array(
-    'favorites:items' => 'Favoritos',
-    'favorites:menu' => 'Favoritos',
+    'favorites:items' => 'Elementos de trabajo',
+    'favorites:menu' => 'Elementos de trabajo',
     'favorites:groups' => 'Grupos Favoritos',
 	'favorites:users' => 'Habitantes favoritos',
-    'favorites:add' => 'Clickea para agregar un elemento favorito',
-    'favorites:remove' => 'Clickea para remover de los favoritos',
-    'favorites:widget:title' => 'Favoritos',
-    'favorites:widget:description' => 'Mostra los elementos favoritos recientes',
-    'favorites:removed' => 'Elemento eliminado de los favoritos',
-    'favorites:removefailed' => 'No se pudo remover el elemento de los favoritos',
-    'favorites:added' => 'Elemento agregado a los favoritos',
-    'favorites:addfailed' => 'No se pudo agregar el elemento a los favoritos',
+    'favorites:add' => 'Clickea para agregar un elemento de trabajo',
+    'favorites:remove' => 'Clickea para eliminar de los elementos de trabajo',
+    'favorites:widget:title' => 'Elementos de trabajo',
+    'favorites:widget:description' => 'Muestra los elementos de trabajo recientes',
+    'favorites:removed' => 'Elemento eliminado de los elementos de trabajo',
+    'favorites:removefailed' => 'No se pudo eliminar el elemento de los favoritos',
+    'favorites:added' => 'Elemento agregado a los elementos de trabajo',
+    'favorites:addfailed' => 'No se pudo agregar el elemento a los elementos de trabajo',
     'favorites:numbertodisplay' => 'Selecciona el número de elementos a mostrar'
 );
 	
-add_translation("es",$es);
+add_translation("es", $es);
