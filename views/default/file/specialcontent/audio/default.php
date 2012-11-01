d9b7710fab6ef2165e1372f719cbd1abd5db7458
hellekin <hellekin@cepheide.org>
2012-11-01 02:19:09 -0300

    Add AGPLv3+ licence

--- views/default/file/specialcontent/audio/default.php
+++ views/default/file/specialcontent/audio/default.php
@@ -1,7 +1,25 @@
 <?php
 /**
- * AudioHTML5 file view override
- * @package ElggAudioHTML5
+ * Audio HTML5 -- File view override
+ *
+ * @package        Lorea
+ * @subpackage     AudioHTML5
+ *
+ * Copyright 2011-2012 Lorea Faeries <federation@lorea.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 echo elgg_view('audio_html5/audioplayer', array('file_guid' => $vars['entity']->getGUID()));
