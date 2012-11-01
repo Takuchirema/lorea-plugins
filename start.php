e42eea3180ad62c2a3555aac95a48aacd0f1f761
hellekin <hellekin@cepheide.org>
2012-11-01 01:44:16 -0300

    Add AGPLv3+ licensing information

--- start.php
+++ start.php
@@ -1,9 +1,28 @@
 <?php
 /**
- * Assemblies
+ * Assemblies -- Essentials for group decision making
  *
- * @package Assemblies
+ * @package        Lorea
+ * @subpackage     Assemblies
+ * @homepage       http://lorea.org/plugin/assemblies
+ * @copyright      2011-2012 Lorea Faeries <federation@lorea.org>
+ * @license        COPYING, http://www.gnu.org/licenses/agpl
  *
+ * Copyright 2012 Lorea Faeries <federation@lorea.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 elgg_register_event_handler('init', 'system', 'assemblies_init');
--- views/default/object/assembly.php
+++ views/default/object/assembly.php
@@ -1,11 +1,28 @@
 <?php
 /**
- * View for assembly object
+ * Assemblies -- View for assembly object
  *
- * @package Assemblies
+ * @package        Lorea
+ * @subpackage     Assemblies
  *
  * @uses $vars['entity']    The assembly object
  * @uses $vars['full_view'] Whether to display the full view
+ *
+ * Copyright 2012 Lorea Faeries <federation@lorea.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 echo elgg_view('crud/object', $vars);
