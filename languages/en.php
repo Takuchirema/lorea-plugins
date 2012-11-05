6177d6bd50c728435b663ee1b59313349959f70c
hellekin <hellekin@cepheide.org>
2012-11-05 11:27:46 -0300

    Update translations

--- languages/en.php
+++ languages/en.php
@@ -1,62 +1,77 @@
 <?php
 /**
- * Elgg spotlight lorea
- * 
- * @package
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
- * @author lorea
- * @copyright lorea
- * @link http://lorea.cc
+ * Spotlight -- Translation strings for English
+ *
+ * @package        Lorea
+ * @subpackage     Spotlight
+ *
+ * Copyright 2011-2012 Lorea Faeries <federation@lorea.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 $english = array(
 
-	'stats:stats' => "Community stats",
-	'stats:members' => "Members",
-	'stats:active' => "active Members",
-	'stats:blogs' => "Blogs",
-	'stats:photos' => "Photos",
-	'stats:groups' => "Groups",
-	'stats:pages' => "Pages",
-	'stats:videos' => "Videos",
-	'stats:files' => "Files",
-	'stats:bookmarks' => "Bookmarks",
-	'stats:current' => "currently has:",
-	'stats:andmore' => "and more...",
-	'stats:public' => "publicly available.",
-	'stats:register' => "Register now,",
-	'stats:access' => "to access more.",	
+	'stats:stats'      => "Community stats",
+	'stats:members'    => "Members",
+	'stats:active'     => "active Members",
+	'stats:blogs'      => "Blogs",
+	'stats:photos'     => "Photos",
+	'stats:groups'     => "Groups",
+	'stats:pages'      => "Pages",
+	'stats:videos'     => "Videos",
+	'stats:files'      => "Files",
+	'stats:tasks'      => "Tasks",
+	'stats:assemblies' => 'Assemblies',
+	'stats:bookmarks'  => "Bookmarks",
+	'stats:current'    => "currently has:",
+	'stats:andmore'    => "and more...",
+	'stats:public'     => "publicly available.",
+	'stats:register'   => "Register now,",
+	'stats:access'     => "to access more.",
 
-	'news:news' => "News",                  
-	'news:features' => "New features",                      
-	'news:planet' => "Planet",                      
+	'news:news'     => "News",
+	'news:features' => "New features",
+	'news:planet'   => "Planet",
 
 	'favorites:favorites' => "Favorites",
 	'favorites:mailserv' => "Groups mail server",
 	'favorites:openid' => "How to: OpenID",
 	'favorites:gpg' => "Notifications with GPG",
 
-	'about:lorea' => "Lorea",
-	'about:blog' => "Main page",
-	'about:group' => "Working group",
-	'lorea:sustainability:url' => "https://n-1.cc/pg/pages/view/14888/",
-	'lorea:sustainability' => "Donations",
+	'about:lorea'               => "Lorea",
+	'about:blog'                => "Website",
+	'about:group'               => "Assembly",
+	'lorea:sustainability:url'  => "https://n-1.cc/pg/pages/view/14888/",
+	'lorea:sustainability'      => "Donations",
 
-	'help:help' => "Help?",
-	'help:faq' => "FAQ / Contact",
-	'help:howto' => "Howtos",
-	'help:group' => "Help group",
+	'help:help'  => "Help?",
+	'help:faq'   => "FAQ",
+	'help:howto' => "Manuals",
+	'help:group' => "Help Group",
 
-	'dev:dev' => "Development",
-	'dev:bughunting' => "Bug hunting",
-	'dev:testers' => "Testers",
-	'dev:network' => "Dev network",
-	'dev:repo' => "Repository",
+	'dev:dev'         => "Development",
+	'dev:group'       => 'Working Group',
+	'dev:source'      => "Source Code",
+	'dev:bughunting'  => "Bug hunting",
+	'dev:testers'     => "Testers",
+	'dev:network'     => "Dev. Seed",
+	'dev:repo'        => "Repository",
 
 	'contact:contact' => "Contact",
-	'contact:mailing' => "Mailing list",			
+	'contact:mailing' => "Mailing list",
 );
 
 add_translation("en",$english);
-
-?>
--- languages/es.php
+++ languages/es.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * Spotlight -- Translation strings for Spanish
+ *
+ * @package        Lorea
+ * @subpackage     Spotlight
+ *
+ * Copyright 2011-2012 Lorea Faeries <federation@lorea.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 $es = array (
   'stats:members' => 'Miembros',
   'stats:active' => 'Miembros activos',
@@ -8,6 +31,8 @@ $es = array (
   'stats:pages' => 'Páginas',
   'stats:videos' => 'Vídeos',
   'stats:files' => 'Ficheros',
+  'stats:assemblies' => 'Asambleas',
+  'stats:tasks' => 'Tareas',
   'stats:bookmarks' => 'Enlaces',
   'stats:stats' => 'Estad&iacute;sticas',
   'stats:current' => 'actualmente como:',
@@ -15,27 +40,36 @@ $es = array (
   'stats:public' => 'disponible públicamente.',
   'stats:register' => 'Regístrate ahora,',
   'stats:access' => 'para acceder a más.',
+
   'news:news' => 'Noticias',
   'news:features' => 'Nuevas funcionalidades',
   'news:planet' => 'Planet',
+
   'favorites:favorites' => 'Favoritos',
   'favorites:mailserv' => 'Listas de correo en grupos',
   'favorites:openid' => 'Cómo usar: OpenID',
   'favorites:gpg' => 'Notificaciones con GPG',
+
   'about:lorea' => 'Lorea',
-  'about:blog' => 'Página principal',
-  'about:group' => 'Grupo de trabajo',
+  'about:blog'  => 'Sitio Web',
+  'about:group' => 'Asamblea',
+
   'lorea:sustainability:url' => 'https://n-1.cc/pg/pages/view/14884/',
   'lorea:sustainability' => 'Donaciones',
-  'help:help' => '¿Ayuda?',
-  'help:faq' => 'FAQ / Contacto',
-  'help:howto' => 'Cómo hacer',
+
+  'help:help'  => '¿Ayuda?',
+  'help:faq'   => 'FAQ',
+  'help:howto' => 'Manuales',
   'help:group' => 'Grupo de ayuda',
-  'dev:dev' => 'Red de desarrollo',
-  'dev:repo' => 'Repositorio',
+
+  'dev:dev'        => 'Red de desarrollo',
+  'dev:group'      => 'Grupo de trabajo',
+  'dev:source'     => 'Fonte de codigo',
+  'dev:repo'       => 'Repositorio',
   'dev:bughunting' => 'Cazadores de bugs',
-  'dev:testers' => 'Testeadores',
-  'dev:network' => 'Comunidad de desarrollo',
+  'dev:testers'    => 'Testeadores',
+  'dev:network'    => 'Comunidad de desarrollo',
+
   'contact:contact' => 'Contacto',
   'contact:mailing' => 'Lista de correo',
 );
