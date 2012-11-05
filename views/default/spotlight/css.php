f47f22ef9053c5e8fee71327c804a80bebe3ca21
hellekin <hellekin@cepheide.org>
2012-11-05 11:31:26 -0300

    Fix CSS column width

--- views/default/spotlight/css.php
+++ views/default/spotlight/css.php
@@ -1,3 +1,30 @@
+<?php
+/**
+ * Spotlight -- Stylesheet
+ *
+ * @package        Lorea
+ * @subpackage     Spotlight
+ * @homepage       http://lorea.org/plugin/spotlight
+ * @copyright      2011-2012 Lorea Faeries <federation@lorea.org>
+ * @license        COPYING, http://www.gnu.org/licenses/agpl
+ *
+ * Copyright 2011-2012 Lorea Faeries <federation@lorea.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+?>
 /* ***************************************
 	SPOTLIGHT
 *************************************** */
@@ -8,6 +35,7 @@
 
 .spotlight-column {
 	display: table-cell;
+	min-width: 22em;
 }
 
 .spotlight-module {
