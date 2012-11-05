58d10f98185540f3f9ec4efa1d72b7b13d1f5f55
Pablo Martin <caedes@sindominio.net>
2012-11-05 08:19:29 +0000

    fix upgrade.

--- upgrades/2012100501.php
+++ upgrades/2012100501.php
@@ -1,5 +1,7 @@
 <?php
 
+global $MIGRATED;
+
 $options = array(
         'type' => 'object',
         'subtypes' => array('microtheme'),
@@ -19,13 +21,23 @@ if (!$topics) {
  * upgrade function, generates thumbnails
  */
 function microthemes_2012100501($microtheme) {
+	$MIGRATED += 1;
+	if ($MIGRATED % 100 == 0) {
+		error_log(" * microtheme user $microtheme->guid");
+	}
+
 	$prefix = "microthemes/banner_".$microtheme->guid;
 	$filehandler = new ElggFile();
+	$filehandler->owner_guid = $microtheme->owner_guid;
 	$filehandler->setFilename($prefix.'_master.jpg');
+	$mime = $filehandler->getMimeType();
+        try {
 	if ($filehandler->exists()) {
 		 microthemes_create_thumbnails($microtheme, $filehandler);
 	}
-	
+	} catch (Exception $e) {
+	}
+	$microtheme->save();
 	$microtheme->deleteMetadata('hidesitename');
 	return true;
 }
@@ -36,13 +48,13 @@ function microthemes_2012100501($microtheme) {
 $options['limit'] = 0;
 
 $previous_access = elgg_set_ignore_access(true);
-$batch = new ElggBatch('elgg_get_entities', $options, "microthemes_2012100501", 100);
+$batch = new ElggBatch('elgg_get_entities_from_metadata', $options, "microthemes_2012100501", 100, false);
 elgg_set_ignore_access($previous_access);
 
 if ($batch->callbackResult) {
-	error_log("Elgg Etherpad microthemes upgrade (201210050) succeeded");
+	error_log("Elgg microthemes upgrade (201210050) succeeded");
 } else {
-	error_log("Elgg Etherpad microthemes upgrade (201210050) failed");
+	error_log("Elgg microthemes upgrade (201210050) failed");
 }
 
 
