abc3e60586a967199d80130fb77e5c9f579115c4
hellekin <hellekin@cepheide.org>
2012-11-05 11:32:01 -0300

    Update license information

--- start.php
+++ start.php
@@ -1,7 +1,28 @@
 <?php
 /**
- * Elgg spotlight plugin
+ * Spotlight -- Lorea spotlight
  *
+ * @package        Lorea
+ * @subpackage     Spotlight
+ * @homepage       http://lorea.org/plugin/spotlight
+ * @copyright      2011-2012 Lorea Faeries <federation@lorea.org>
+ * @license        COPYING, http://www.gnu.org/licenses/agpl
+ *
+ * Copyright 2011-2012 Lorea Faeries <federation@lorea.org>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 elgg_register_event_handler('init','system','spotlight_init');
