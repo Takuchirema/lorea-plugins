07b945c0322b73e8c565a5371ca440d9df25e231
hellekin <hellekin@cepheide.org>
2012-11-05 14:31:37 -0300

    Add translation string for copyright

--- languages/en.php
+++ languages/en.php
@@ -65,6 +65,24 @@ $english = array(
 	'dev:dev'         => "Development",
 	'dev:group'       => 'Working Group',
 	'dev:source'      => "Source Code",
+	'dev:source:code' => '
+<p>Lorea is free software.</p>
+
+<p>Copyright 2009-2012 Lorea Faeries &lt;federation@lorea.org&gt;</p>
+
+<p>This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.</p>
+
+<p>This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Affero General Public License for more details.</p>
+
+<p>You should have received a copy of the GNU Affero General Public License along with this program. If not, see &lt;<a href="http://www.gnu.org/licenses/agpl">http://www.gnu.org/licenses/</a>&gt;.</p>
+
+',
 	'dev:bughunting'  => "Bug hunting",
 	'dev:testers'     => "Testers",
 	'dev:network'     => "Dev. Seed",
