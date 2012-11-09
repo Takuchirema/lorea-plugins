fffc048efac848c4a62c194925e1a0071a112325
hellekin <hellekin@cepheide.org>
2012-11-08 22:16:44 -0300

    Finalize Copyright information

--- views/default/audio_html5/audioplayer.php
+++ views/default/audio_html5/audioplayer.php
@@ -7,12 +7,14 @@
  *
  * Copyright 2011-2012 Lorea Faeries <federation@lorea.org>
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * This file is part of the AudioHTML5 plugin for Elgg.
  *
- * This program is distributed in the hope that it will be useful, but
+ * AudioHTML5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * AudioHTML5 is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Affero General Public License for more details.
