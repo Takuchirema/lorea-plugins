61b94441eb20e59296e638a73b244341938242a2
hellekin <hellekin@cepheide.org>
2012-11-08 22:05:22 -0300

    Finalize Copyright information

--- views/default/object/agenda_point.php
+++ views/default/object/agenda_point.php
@@ -2,20 +2,22 @@
 /**
  * Assemblies -- View for agenda point object
  *
- * @package        Lorea
- * @subpackage     Assemblies
- *
  * @uses $vars['entity']    The agenda point object
  * @uses $vars['full_view'] Whether to display the full view
  *
+ * @package        Lorea
+ * @subpackage     Assemblies
+ *
  * Copyright 2012 Lorea Faeries <federation@lorea.org>
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * This file is part of the Assemblies plugin for Elgg.
+ * 
+ * Assemblies is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
+ * Assemblies is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Affero General Public License for more details.
@@ -23,7 +25,6 @@
  * You should have received a copy of the GNU Affero General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
- */
  */
 
 echo elgg_view('crud/object', $vars);
