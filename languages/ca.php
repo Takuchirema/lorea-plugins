b99ec20d5e37541ea15adcf5604a7eb52fd898a0
Sem <sembrestels@riseup.net>
2012-11-30 23:55:29 +0100

    Added catalan language.

---
+++ languages/ca.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Catalan favorites translations
+ */
+
+$ca = array(
+    'favorites:items' => 'Elements etiquetats',
+    'favorites:menu' => 'Elements etiquetats',
+    'favorites:groups' => 'Grups etiquetats',
+	'favorites:users' => 'Habitants etiquetats',
+    'favorites:add' => 'Fes clic per etiquetar',
+    'favorites:remove' => 'Fes clic per eliminar una etiqueta',
+    'favorites:widget:title' => 'Elements etiquetats',
+    'favorites:widget:description' => 'Mostra els elements etiquetats recents',
+    'favorites:removed' => 'Etiqueta eliminada de l\'element',
+    'favorites:removefailed' => 'No s\'ha pogut eliminar l\'etiqueta',
+    'favorites:added' => 'Element afegit als elements etiquetats',
+    'favorites:addfailed' => 'No s\'ha pogut afegir l\'etiqueta a l\'element',
+    'favorites:numbertodisplay' => 'Sel·lecciona el número d\'elements a mostrar'
+);
+	
+add_translation("ca", $ca);
