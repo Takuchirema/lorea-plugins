7cd80b2da2cd2dc81388d0a4c3c1397ca9bd408d
Sem <sembrestels@riseup.net>
2013-11-10 07:06:03 +0100

    Added help to subtree.sh

--- subtree.sh
+++ subtree.sh
@@ -4,6 +4,12 @@ command=$1
 branch=$2
 plugin=$3
 
+if [ ! $command ] || [ ! $branch ]; then
+  echo "  Usage:   ./subtree.sh <command> <branch> [<plugin-name>]
+  Example: ./subtree.sh pull develop etherpad"
+  exit
+fi
+
 git checkout $branch
 if [ $plugin ]; then
   git subtree $command --prefix mod/$plugin git@gitorious.org:lorea/$plugin.git $branch
